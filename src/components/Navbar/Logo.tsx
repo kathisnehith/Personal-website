99f6f6fbe018dfb73d35a8950d59f397945db39d
Snehith Reddy Kathi <kathisnehithreddy@gmail.com>
2025-01-25 23:23:21 -0600

    Update Logo.tsx

--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -5,7 +5,7 @@ interface LogoProps {
 export function Logo({ onClick }: LogoProps) {
   return (
     <img
-      src="/src/assets/images/logo.png"
+      src="/logo.png"
       alt="Logo"
       className="h-[40px] md:h-[50px] w-auto cursor-pointer hover:opacity-80 transition-opacity"
       onClick={onClick}
